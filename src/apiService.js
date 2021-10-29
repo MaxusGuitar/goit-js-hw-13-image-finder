c8c347fa3d9540acc3a0239e66e1137dc39239b3
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-10-29 23:30:58 +0300

    search is ready

--- src/apiService.js
+++ src/apiService.js
@@ -5,15 +5,24 @@ export default class ApiService {
     }
 
     fetchArticles() {
-        console.log(this);
-        fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.querySearch}
+        
+        return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.querySearch}
         &page=${this.page}&per_page=12&key=24079663-849aadf309a059b421030ae2f`)
-    .then(a => a.json())
-            .then(n => {
-        this.page += 1 // увеличивает страницу с данными на 1 при нажатии на кнопку ЗАГРУЗИТЬ БОЛЬШЕ
-    })
+        .then(a => a.json())
+        .then(n => { 
+            this.plusPage()
+            return n.hits
+                    })
     }
 
+    plusPage() {
+        this.page += 1 // увеличивает страницу с данными на 1 при нажатии на кнопку ЗАГРУЗИТЬ БОЛЬШЕ
+    }
+
+    resetPage() {
+        this.page = 1 // для сброса стр при новом поиске
+    }
+    
     get query() {
     return this.querySearch
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import './sass/main.scss';
 import ApiService from './apiService';
+import photos from './templates/photos.hbs'
 
 const refs = {
     searchform: document.querySelector('.search'),
-    loadBut: document.querySelector('.show')
+    loadBut: document.querySelector('.show'),
+    photosCard: document.querySelector('.gallery')
 }
 const apiService = new ApiService()
 
@@ -14,9 +16,16 @@ function onSearch(e) {
     e.preventDefault()
 
     apiService.query = e.currentTarget.elements.query.value
+    apiService.resetPage()
     apiService.fetchArticles()
+        .then(addPhotos) 
 }
 
 function onLoad() {
     apiService.fetchArticles()
+        .then(addPhotos)
+}
+
+function addPhotos(hits) {
+    refs.photosCard.insertAdjacentHTML('beforeend', photos(hits))
 }
