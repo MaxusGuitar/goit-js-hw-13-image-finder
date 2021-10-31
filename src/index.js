9a99afc947e2a7bbc47031214d90ea2fa372bbf3
MaxusGuitar <maxim1999chikhanov@gmail.com>
2021-10-31 15:50:15 +0200

    aaaaaaa

--- src/index.js
+++ src/index.js
@@ -1,31 +1,52 @@
 import './sass/main.scss';
 import ApiService from './apiService';
 import photos from './templates/photos.hbs'
+import LoadMoreBtn from './loadmore.js'
 
 const refs = {
     searchform: document.querySelector('.search'),
-    loadBut: document.querySelector('.show'),
     photosCard: document.querySelector('.gallery')
 }
 const apiService = new ApiService()
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+})
 
 refs.searchform.addEventListener('submit', onSearch)
-refs.loadBut.addEventListener('click', onLoad)
+//refs.loadBut.addEventListener('click', onLoad)
+loadMoreBtn.refs.button.addEventListener('click', onLoad)
 
 function onSearch(e) {
     e.preventDefault()
 
+    loadMoreBtn.show() // показывается текст загрузки кнопки
+    loadMoreBtn.disable() //кнопка неактивна
+    
     apiService.query = e.currentTarget.elements.query.value
     apiService.resetPage()
     apiService.fetchArticles()
-        .then(addPhotos) 
+        .then(f => {
+            clearPhotoCard() // очищает стр после каждого нового запроса
+            addPhotos(f)
+        })
+    loadMoreBtn.enable() // после результата запроса кнопка снова активна
 }
 
 function onLoad() {
+    loadMoreBtn.disable() //кнопка неактивна
     apiService.fetchArticles()
-        .then(addPhotos)
+        .then(f => {
+            addPhotos(f)
+            loadMoreBtn.enable() // после результата запроса кнопка снова активна
+        })
+    
 }
 
 function addPhotos(hits) {
     refs.photosCard.insertAdjacentHTML('beforeend', photos(hits))
+}
+
+function clearPhotoCard() {
+    refs.photosCard.innerHTML = '' // для очищения результата запроса на стр
 }
